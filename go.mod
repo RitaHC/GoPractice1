a4a438adf60ebf004329498349d2c758021f66f6
RitaHC <ritahameerchauhan@ritas-air.mynetworksettings.com>
2024-02-16 20:44:23 -0500

    commit

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/RitaHC/GoPractice1
 
 go 1.20
+
+require (
+	github.com/RitaHC/Algorithm-Practice v0.0.0-20240217010707-274b85f6dd32 // indirect
+	github.com/RitaHC/mystrings v0.0.0-20240217012800-8fb13015d22b // indirect
+)
--- main.go
+++ main.go
@@ -5,10 +5,19 @@ package main
 // Recompile everytime before running the code
 // go build && ./FileName
 
+///////////////// IMPORTS ///////////
+// 1) Copy Path name
+// 2) use the package.Function(parameter)
+// 3) go get path
+
 import (
 	"fmt"
+
+	"github.com/RitaHC/mystrings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(mystrings.Reverse("Hello World"))
+	fmt.Println("------------------------------")
+	// fmt.Println()
 }
